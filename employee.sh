78aff088809fe8c1aa471d58d119f85c8ad7be60
shital gawade <shitalgawade131@gmail.com>
2022-12-31 00:17:44 +0530

    new branch1

--- employee.sh
+++ employee.sh
@@ -2,3 +2,12 @@
 
 echo "Welcome to Employee Wage Computation Program"
 
+isPresent=1
+Attendance=$(($RANDOM%2))
+
+if [[ $isPresent -eq $Attendance ]]
+then
+    echo "Employee is present"
+else
+     echo "Employee is abscent"
+fi
